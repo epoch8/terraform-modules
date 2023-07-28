e30eda8dffc8e7ae529b44518bc58e9590c02dad
Andrey Tatarinov <a@tatarinov.co>
2023-07-28 12:20:33 +0000

    k8s-kuberay: initial implementation

---
+++ k8s-kuberay/main.tf
@@ -0,0 +1,25 @@
+variable "k8s_kuberay_namespace" {
+  description = "Namespace to deploy to"
+  type        = string
+  default     = "kuberay"
+}
+
+# create namespace
+resource "kubernetes_namespace" "kuberay" {
+  metadata {
+    name = var.k8s_kuberay_namespace
+  }
+}
+
+# deploy kuberay operator
+resource "helm_release" "kuberay" {
+  name       = "kuberay"
+  repository = "https://ray-project.github.io/kuberay-helm/"
+  chart      = "kuberay-operator"
+  namespace  = kubernetes_namespace.kuberay.metadata[0].name
+  version    = "0.6.0"
+  values = [
+    jsonencode({
+    })
+  ]
+}
